dc63e64d1bd662c250e1fd843030b35e84c57048
Cory Chilton <coryhookchilton@gmail.com>
2023-10-17 21:08:39 -0700

    Add navigation bar

--- src/index.js
+++ src/index.js
@@ -1,4 +1,20 @@
 import home from './home.js';
 import menu from './menu.js';
 
-home();
+const displayController = (function(){
+  const homeButton = document.getElementById('home-button');
+  const menuButton = document.getElementById('menu-button');
+  const contactButton = document.getElementById('contact-button');
+  const contentDiv = document.getElementById('content');
+  homeButton.addEventListener('click', () => {clearContent(); home();});
+  menuButton.addEventListener('click', () =>{clearContent(); menu();});
+  
+
+  // menuButton.addEventListener('click', menuClick);
+
+  function clearContent(){
+    contentDiv.textContent = "";
+  }
+})();
+
+menu();
