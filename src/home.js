23f15177dd2e2500b51ff5846d2e6b2943a25ed5
Cory Chilton <coryhookchilton@gmail.com>
2023-10-17 20:47:05 -0700

    Add html-webpack-plugin and asset + style management

--- src/home.js
+++ src/home.js
@@ -1,9 +1,11 @@
+import Nachos from './nachos.jpg';
+
 export default function home() {
   const contentDiv = document.querySelector('div#content');
   const titleH1 = document.createElement('h1');
   titleH1.textContent = 'Nachoria';
   const nachosImg = document.createElement('img');
-  nachosImg.setAttribute('src', '/src/nachos.jpg');
+  nachosImg.setAttribute('src', Nachos);
   nachosImg.setAttribute('alt', 'nachos');
   const tagH2 = document.createElement('h2');
   tagH2.textContent = 'The best nachos in the world!';
